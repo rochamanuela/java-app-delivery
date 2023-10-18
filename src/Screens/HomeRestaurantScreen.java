0cf812189818bb6b29d938ef9a2b821809cb98da
rochamanuela <rochadesouzamanuela@gmail.com>
2023-10-18 13:42:20 -0300

    18/10/2023 - 13:42

--- src/Screens/HomeRestaurantScreen.java
+++ src/Screens/HomeRestaurantScreen.java
@@ -1,4 +1,67 @@
 package Screens;
+import Components.Button;
 
-public class HomeRestaurantScreen {
+import java.awt.Container;
+import java.util.Objects;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class HomeRestaurantScreen extends JFrame {
+    Button btnDish, btnResume, btnHome;
+    JLabel background;
+
+    public HomeRestaurantScreen(){
+        super("App Delivery - Home");
+        initializeComponents();
+    }
+
+    public Button createButton(JLabel label, int x, int y, int width, int height) {
+        Button button = new Button();
+        button.setBounds(x, y, width, height);
+        label.add(button);
+        return button;
+    }
+
+    private void initializeComponents(){
+        Container frame = getContentPane();
+        frame.setLayout(null);
+
+        ImageIcon wallpaper = new ImageIcon(Objects.requireNonNull(getClass().getResource("../Images/inicial_restaurante.png")));
+        background = new JLabel(wallpaper);
+        background.setBounds(-5, -20, 656, 480);
+        frame.add(background);
+
+        btnHome = createButton(background, 40, 37, 34, 34);
+        btnDish = createButton(background, 329, 219, 201, 35);
+        btnResume = createButton(background, 329, 264, 201, 35);
+
+        // Add action listeners to the buttons
+        btnHome.addActionListener(e -> showHomeScreen());
+        btnDish.addActionListener(e -> showRegisterDishScreen());
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(656, 480);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    private void showHomeScreen() {
+        dispose(); // Close the current screen
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.setVisible(true);
+    }
+
+    private void showRegisterDishScreen() {
+        dispose(); // Close the current screen
+        RegisterDishScreen registerDishScreen = new RegisterDishScreen();
+        registerDishScreen.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(HomeRestaurantScreen::new);
+    }
 }
--- src/Screens/RegisterDishScreen.java
+++ src/Screens/RegisterDishScreen.java
@@ -1,4 +1,71 @@
 package Screens;
+import Components.Button;
+import Components.Input;
 
-public class RegisterDishScreen {
+import java.awt.Container;
+import java.util.Objects;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class RegisterDishScreen extends JFrame {
+    Button btnAddDish, btnHome;
+    Input nameDishInput, priceDishInput;
+    JLabel background;
+
+    public RegisterDishScreen(){
+        super("App Delivery - Dish");
+        initializeComponents();
+    }
+
+    public Input createInput (JLabel label, int x, int y, int width, int height, String text) {
+        Input input = new Input(text);
+        input.setBounds(x, y, width, height);
+        label.add(input);
+        return input;
+    }
+
+    public Button createButton(JLabel label, int x, int y, int width, int height) {
+        Button button = new Button();
+        button.setBounds(x, y, width, height);
+        label.add(button);
+        return button;
+    }
+
+    private void initializeComponents(){
+        Container frame = getContentPane();
+        frame.setLayout(null);
+
+        ImageIcon wallpaper = new ImageIcon(Objects.requireNonNull(getClass().getResource("../Images/cadastrar_prato.png")));
+        background = new JLabel(wallpaper);
+        background.setBounds(-5, -20, 656, 480);
+        frame.add(background);
+
+        nameDishInput = createInput(background, 105, 188, 163, 35, "");
+        priceDishInput = createInput(background, 290, 188, 107, 35, "");
+
+        btnHome = createButton(background, 40, 37, 34, 34);
+        btnAddDish = createButton(background, 405, 183, 143, 35);
+
+        // Add action listeners to the buttons
+        btnHome.addActionListener(e -> showHomeScreen());
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(656, 480);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    private void showHomeScreen() {
+        dispose(); // Close the current screen
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(RegisterDishScreen::new);
+    }
 }
