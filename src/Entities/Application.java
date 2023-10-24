9afe30d1199377f600d71afc683605c8ed533446
rochamanuela <rochadesouzamanuela@gmail.com>
2023-10-24 16:27:35 -0300

    24/10/2023 - 16:27

---
+++ src/Entities/Application.java
@@ -0,0 +1,67 @@
+/*package Entities;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import Screens.HomeScreen;
+import Screens.LoginClientScreen;
+
+public class Application {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(HomeScreen::new);
+
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+        ArrayList<Client> clients = new ArrayList<>();
+        ArrayList<Dish> dishs = new ArrayList<>();
+
+        // public void registerRestaurant() { }
+        // public void registerClient() { }
+
+        Client client1 = new Client("João", "123456789", "senha123", 0, 0);
+        clients.add(client1);
+
+        Client client2 = new Client("Maria", "987654321", "senha456", 0, 0);
+        clients.add(client2);
+
+        System.out.println("vc aguenta");
+    }
+}*/
+
+package Entities;
+
+import Screens.LoginClientScreen;
+
+import java.util.ArrayList;
+
+public class Application {
+    public static ArrayList<Client> clients = new ArrayList<>();
+    public static ArrayList<Restaurant> restaurants = new ArrayList<>();
+
+    public static Restaurant restaurantSession;
+    public static Client clientSession;
+
+    public static ArrayList<Dish> dishes = new ArrayList<>();
+    
+    public Application() {
+        /* code arprigio 
+        LoginScreen start = new LoginScreen();
+        start.openLoginScreen();
+        User userAdmin = new User("adminClient", "admin123", 0, 0);
+        Restaurant resAdmin = new Restaurant("adminRestaurant", "res123", 0, 0);
+        clients.add(userAdmin);
+        restaurants.add(resAdmin);*/
+
+        LoginClientScreen loginClient = new LoginClientScreen();
+        loginClient.initializeComponents();
+
+        Client defaultClient = new Client(null, "adminClient" , "admin123", 0, 0);
+        Restaurant defaultRestaurant = new Restaurant(null, "adminRestaurant", "admin123", 0, 0);
+
+        clients.add(defaultClient);
+        restaurants.add(defaultRestaurant);
+
+    }
+}
--- src/Screens/LoginClientScreen.java
+++ src/Screens/LoginClientScreen.java
@@ -1,8 +1,11 @@
 package Screens;
+
 import Components.Button;
 import Components.Input;
+import Entities.Client;
 
 import java.awt.Container;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import javax.swing.ImageIcon;
@@ -12,12 +15,15 @@ import javax.swing.SwingUtilities;
 
 public class LoginClientScreen extends JFrame {
     Button btnLogin, btnHome, btnInfos;
-    Input cnpjInput, senhaInput;
+    Input cnpjInput, passwordInput;
     JLabel background;
 
-    public LoginClientScreen(){
+    public LoginClientScreen() {
         super("App Delivery - Login Client");
         initializeComponents();
+    }
+
+    public LoginClientScreen(ArrayList<Client> clients) {
     }
 
     public Button createButton(JLabel label, int x, int y, int width, int height) {
@@ -27,33 +33,56 @@ public class LoginClientScreen extends JFrame {
         return button;
     }
 
-    public Input createInput (JLabel label, int x, int y, int width, int height, String text) {
+    public Input createInput(JLabel label, int x, int y, int width, int height, String text) {
         Input input = new Input(text);
         input.setBounds(x, y, width, height);
         label.add(input);
         return input;
     }
 
-    private void initializeComponents(){
+    public void initializeComponents() {
         Container frame = getContentPane();
         frame.setLayout(null);
 
-        ImageIcon wallpaper = new ImageIcon(Objects.requireNonNull(getClass().getResource("../Images/login_do_cliente.png")));
+        ImageIcon wallpaper = new ImageIcon(
+                Objects.requireNonNull(getClass().getResource("../Images/login_do_cliente.png")));
         background = new JLabel(wallpaper);
         background.setBounds(-5, -20, 656, 480);
         frame.add(background);
 
         String placeholderText = "Digite algo aqui";
         cnpjInput = createInput(background, 235, 181, 190, 35, placeholderText);
-        senhaInput = createInput(background, 235, 246, 190, 35, "senha");
+        passwordInput = createInput(background, 235, 246, 190, 35, "senha");
 
         btnLogin = createButton(background, 219, 301, 201, 35);
         btnHome = createButton(background, 40, 37, 34, 34);
         btnInfos = createButton(background, 515, 419, 111, 24);
 
-        // Add action listeners to the buttons
+        // Add action listeners to the "static" buttons
         btnHome.addActionListener(e -> showHomeScreen());
         btnInfos.addActionListener(e -> showInfosScreen());
+
+        // Add action listener to functional buttos: Login
+        btnLogin.addActionListener(e -> {
+            String cpf = cnpjInput.getText();
+            String password = passwordInput.getText();
+
+            System.out.println("CPF: " + cpf + " Senha: " + password);
+
+            if (isLoginValid(cpf, password, null)) {
+                // Login bem-sucedido
+                // Execute ação de login
+                // Por exemplo: new HomeScreen(authenticatedClient).setVisible(true);
+                dispose(); // Close the current screen
+                InfosScreen infosScreen = new InfosScreen();
+                infosScreen.setVisible(true);
+                System.out.println("o login deu certo");
+            } else {
+                // Login falhou
+                // Exiba uma mensagem de erro ou ação apropriada
+                System.out.println("o login deu ruim");
+            }
+        });
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setSize(656, 480);
@@ -61,6 +90,23 @@ public class LoginClientScreen extends JFrame {
         setLayout(null);
         setVisible(true);
     }
+
+    public boolean isLoginValid(String cpf, String password, ArrayList<Client> clients) {
+        if (cpf == null || password == null) {
+            throw new IllegalArgumentException("Null arguments are not allowed.");
+        }
+    
+        for (Client client : clients) {
+            if (client != null && client.getCpf() != null && client.getPassword() != null) {
+                if (client.getCpf().equals(cpf) && client.getPassword().equals(password)) {
+                    return true; // Autenticação bem-sucedida
+                }
+            }
+        }
+    
+        return false; // Autenticação falhou
+    }
+    
 
     private void showHomeScreen() {
         dispose(); // Close the current screen
